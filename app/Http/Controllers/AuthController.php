6439b244cf957d15c0bc3c8b4e7fb0bf884c6bf6
krisna <krisnamadani@gmail.com>
2024-01-17 21:21:01 +0700

    authentication

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,5 +6,26 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function loginView()
+    {
+        return view('login');
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (auth()->attempt($credentials)) {
+            return redirect()->route('home');
+        }
+
+        return redirect()->back()->with('error', 'Invalid credentials');
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+
+        return redirect()->route('login');
+    }
 }
