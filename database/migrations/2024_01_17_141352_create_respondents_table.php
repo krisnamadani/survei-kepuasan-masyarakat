67ab53d7891111c42f5b55f347a0716d458c6ff1
krisna <krisnamadani@gmail.com>
2024-01-19 06:25:36 +0700

    migration

--- database/migrations/2024_01_17_141352_create_respondents_table.php
+++ database/migrations/2024_01_17_141352_create_respondents_table.php
@@ -15,6 +15,23 @@ class CreateRespondentsTable extends Migration
     {
         Schema::create('respondents', function (Blueprint $table) {
             $table->id();
+            $table->string('gender');
+            $table->string('age');
+            $table->string('email')->nullable();
+            $table->string('education');
+            $table->string('occupation');
+            $table->string('institution')->nullable();
+            $table->string('service')->nullable();
+            $table->string('answer1');
+            $table->string('answer2');
+            $table->string('answer3');
+            $table->string('answer4');
+            $table->string('answer5');
+            $table->string('answer6');
+            $table->string('answer7');
+            $table->string('answer8');
+            $table->string('answer9');
+            $table->string('comment')->nullable();
             $table->timestamps();
         });
     }
