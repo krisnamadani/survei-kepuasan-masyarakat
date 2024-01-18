414fb56e01fd2679ea4d888923040d1c62340849
krisna <krisnamadani@gmail.com>
2024-01-18 22:45:25 +0700

    auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,17 +6,17 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function loginView()
+    public function login()
     {
         return view('login');
     }
 
-    public function login(Request $request)
+    public function loginPost(Request $request)
     {
         $credentials = $request->only('email', 'password');
 
         if (auth()->attempt($credentials)) {
-            return redirect()->route('home');
+            return redirect()->route('admin.dashboard');
         }
 
         return redirect()->back()->with('error', 'Invalid credentials');
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', '\App\Http\Controllers\AuthController@login')->name('login');
+Route::post('/login', '\App\Http\Controllers\AuthController@loginPost')->name('login.post');
+Route::get('/logout', '\App\Http\Controllers\AuthController@logout')->name('logout');
